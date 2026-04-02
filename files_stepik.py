294b4fb13be9c225d817bbaee261bda2ff5bb807
Sergey <seregasebas@gmail.com>
2026-04-02 14:30:20 +0700

    from mac

--- files_stepik.py
+++ files_stepik.py
@@ -1,24 +1,56 @@
 from typing import Any
+from string import punctuation
 
 print('Hello, chuvak!')
 print(25 * '-')
 '---------------------------------------------'
 
-
-def sum_columns(file_name: str) -> list[int]:
-    lst = []
-    with open(file_name, 'r') as f:
-        for line in f:
-            lst.append([num for num in map(int, line.split())])
+def get_best_student(filename: str) -> str:
+    best_student = ''
+    score = 0
+    f = open(filename, 'r', encoding='utf-8')
+    for line in f:
+        line = line.strip().split()
+        if int(line[2]) >= score:
+            score = int(line[2])
+            best_student = f'{line[1].replace(":", "")} {line[0]}'
     f.close()
-    return list(map(sum, zip(*lst)))
+    return best_student
 
-print(sum_columns('отчет.txt'))
+print(get_best_student('files/top_score_2.txt'))
 
-a = [34, 6, 55]
-b = [22, 56, 77]
-print(list(zip(a, b)))
-
+'---------------------------------------------'
+# print(punctuation)
+#
+# def longest_word_in_file(filename: str) -> str:
+#     len_word = 0
+#     word_res = ''
+#     with open(filename, 'r', encoding='utf-8') as f:
+#         for line in f:
+#             if len(line) > 1:
+#                 for word in line.split():
+#                     word = ''.join(w for w in word if w not in punctuation)
+#                     if len(word) >= len_word:
+#                         len_word = len(word)
+#                         word_res = word
+#             else:
+#                 break
+#     return word_res
+#
+# print(longest_word_in_file('files/english_text.txt'))
+'---------------------------------------------'
+# def sum_columns(file_name: str) -> list[int]:
+#     lst = []
+#     with open(file_name, 'r') as f:
+#         for line in f:
+#             lst.append([num for num in map(int, line.split())])
+#     return list(map(sum, zip(*lst)))
+#
+# print(sum_columns('отчет.txt'))
+#
+# a = [34, 6, 55]
+# b = [22, 56, 77]
+# print(list(zip(a, b)))
 '---------------------------------------------'
 # def remove_punctuation(text: str) -> str:
 #     punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
@@ -37,7 +69,6 @@ print(list(zip(a, b)))
 #         for line in f:
 #             for word in line.split():
 #                 lst.append(remove_punctuation(word.lower()))
-#     f.close()
 #     return sorted(set(lst))
 #
 # print(get_unique_words('test_2.txt'))
@@ -50,7 +81,6 @@ print(list(zip(a, b)))
 #             if len(line.split()) >= len_str:
 #                 res = line
 #                 len_str = len(line.split())
-#     f.close()
 #     return res
 # print(most_talkative_line('test_2.txt'))
 '---------------------------------------------'
@@ -66,14 +96,12 @@ print(list(zip(a, b)))
 #     with open(file_name, 'r') as f:
 #         for i in range(n):
 #             print(f.readline().strip())
-#     f.close()
 '---------------------------------------------'
 # def print_first_and_last_line(name_files: str) -> None:
 #     with open(name_files, 'r') as f:
 #         res = f.readlines()
 #         print(res[0].strip())
 #         print(res[-1].strip())
-#     f.close()
 #
 # print_first_and_last_line('test_2.txt')
 '---------------------------------------------'
@@ -81,7 +109,6 @@ print(list(zip(a, b)))
 #     with open(name_file, 'r') as f:
 #         print(f.readline().strip())
 #         print(f.readline().strip())
-#     f.close()
 #
 # print_first_two_lines(name_file='test_2.txt')
 '---------------------------------------------'
@@ -121,6 +148,7 @@ print(list(zip(a, b)))
 #     count = 0
 #     for line in file:
 #         count += 1
+#     file.close()
 #     return count
 #
 # print(count_lines_in_file('test_2.txt'))
